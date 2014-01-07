f8176fffec3f32ce0b8d68b8db85e44d57a30929
Takaaki Kakinuma <takaaki.kakinuma@gmail.com>
2014-01-07 12:07:46 +0900

    Initial Commit

---
+++ TableView/TKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TKAppDelegate.h
+//  TableView
+//
+//  Created by Takaaki Kakinuma on 1/7/14.
+//  Copyright (c) 2014 Takaaki Kakinuma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TableView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TableView
+//
+//  Created by Takaaki Kakinuma on 1/7/14.
+//  Copyright (c) 2014 Takaaki Kakinuma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TKAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TKAppDelegate class]));
+    }
+}
